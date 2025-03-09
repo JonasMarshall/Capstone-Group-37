336a211de439f3ca2a8c7d7aeaef89d8a471b811
19jef6 <119008875+19jef6@users.noreply.github.com>
2025-03-09 16:47:13 -0400

    Update 498_Project_Test.ino

--- 498_Project_Test/498_Project_Test.ino
+++ 498_Project_Test/498_Project_Test.ino
@@ -3,6 +3,8 @@
 #include "DEV_Config.h"
 #include "Debug.h"
 #include "ImageData.h"
+#include <Arduino_LSM9DS1.h>
+#include "Stroke_Detector.h"
 
 int screen = 0;
 int selection = 0;
@@ -18,30 +20,31 @@ unsigned long hours;
 bool started = false;
 int count = 0;
 
-const int B1Pin = 2;
-int B1State = 0;
-const int B2Pin = 2;
-int B2State = 0;
-const int B3Pin = 2;
-int B3State = 0;
-const int B4Pin = 2;
-int B4State = 0;
-const int B5Pin = 2;
-int B5State = 0;
-const int B6Pin = 2;
-int B6State = 0;
+float avgAcceleration;
+// const int B1Pin = 2;
+// int B1State = 0;
+// const int B2Pin = 2;
+// int B2State = 0;
+// const int B3Pin = 2;
+// int B3State = 0;
+// const int B4Pin = 2;
+// int B4State = 0;
+// const int B5Pin = 2;
+// int B5State = 0;
+// const int B6Pin = 2;
+// int B6State = 0;
 
 float data[][2] = {{0.000,1.000},{0.102,0.472},{0.204,-0.369},{0.306,-0.734},{0.408,-0.392},{0.510,0.228},{0.612,0.535},{0.714,0.320},{0.816,-0.135},{0.918,-0.388},{1.020,-0.256},{1.122,0.074},{1.224,0.279},{1.327,0.203},{1.429,-0.035},{1.531,-0.199},{1.633,-0.159},{1.735,0.012},{1.837,0.141},{1.939,0.124},{2.041,0.002},{2.143,-0.099},{2.245,-0.095},{2.347,-0.009},{2.449,0.069},{2.551,0.073},{2.653,0.012},{2.755,-0.048},{2.857,-0.055},{2.959,-0.013},{3.061,0.033},{3.163,0.041},{3.265,0.013},{3.367,-0.022},{3.469,-0.031},{3.571,-0.011},{3.673,0.014},{3.776,0.023},{3.878,0.010},{3.980,-0.009},{4.082,-0.017},{4.184,-0.008},{4.286,0.006},{4.388,0.012},{4.490,0.007},{4.592,-0.004},{4.694,-0.009},{4.796,-0.006},{4.898,0.002},{5.000,0.007}};
-// float data[][2] = {{0.00000,1.00000},{0.05051,0.83205},{0.10101,0.48068},{0.15152,0.04780},{0.20202,-0.35496},{0.25253,-0.63390},{0.30303,-0.73401},{0.35354,-0.64846},{0.40404,-0.41562},{0.45455,-0.10547},{0.50505,0.20018},{0.55556,0.42845},{0.60606,0.53204},{0.65657,0.49808},{0.70707,0.34792},{0.75758,0.12876},{0.80808,-0.10023},{0.85859,-0.28319},{0.90909,-0.38064},{0.95960,-0.37745},{1.01010,-0.28404},{1.06061,-0.13148},{1.11111,0.03795},{1.16162,0.18200},{1.21212,0.26857},{1.26263,0.28240},{1.31313,0.22718},{1.36364,0.12278},{1.41414,-0.00103},{1.46465,-0.11271},{1.51515,-0.18662},{1.56566,-0.20867},{1.61616,-0.17857},{1.66667,-0.10852},{1.71717,-0.01919},{1.76768,0.06617},{1.81818,0.12746},{1.86869,0.15229},{1.91919,0.13821},{1.96970,0.09234},{2.02020,0.02873},{2.07071,-0.03566},{2.12121,-0.08531},{2.17172,-0.10973},{2.22222,-0.10549},{2.27273,-0.07635},{2.32323,-0.03171},{2.37374,0.01625},{2.42424,0.05571},{2.47475,0.07801},{2.52525,0.07946},{2.57576,0.06172},{2.62626,0.03088},{2.67677,-0.00441},{2.72727,-0.03525},{2.77778,-0.05467},{2.82828,-0.05911},{2.87879,-0.04895},{2.92929,-0.02803},{2.97980,-0.00239},{3.03030,0.02135},{3.08081,0.03770},{3.13131,0.04343},{3.18182,0.03820},{3.23232,0.02431},{3.28283,0.00592},{3.33333,-0.01212},{3.38384,-0.02552},{3.43434,-0.03151},{3.48485,-0.02937},{3.53535,-0.02039},{3.58586,-0.00738},{3.63636,0.00614},{3.68687,0.01690},{3.73737,0.02257},{3.78788,0.02228},{3.83838,0.01668},{3.88889,0.00761},{3.93939,-0.00241},{3.98990,-0.01089},{4.04040,-0.01594},{4.09091,-0.01668},{4.14141,-0.01336},{4.19192,-0.00715},{4.24242,0.00018},{4.29293,0.00676},{4.34343,0.01109},{4.39394,0.01234},{4.44444,0.01051},{4.49495,0.00634},{4.54545,0.00104},{4.59596,-0.00399},{4.64646,-0.00758},{4.69697,-0.00901},{4.74747,-0.00815},{4.79798,-0.00541},{4.84848,-0.00163},{4.89899,0.00217},{4.94949,0.00509},{5.00000,0.00650}};
 
 
 void setup() {
   // put your setup code here, to run once:
-  System_Init();
   Serial.begin(9600);
+  Serial.print("Test");
+  System_Init();
   if(USE_IIC) {
     Serial.print("Only USE_SPI_4W, Please revise DEV_config.h !!!");
-    return 0;
+    return;
   }
 
   Serial.print(F("OLED_Init()...\r\n"));
@@ -52,11 +55,27 @@ void setup() {
   Serial.print("Paint_NewImage\r\n");
   Paint_NewImage(BlackImage, OLED_1in5_RGB_WIDTH, OLED_1in5_RGB_HEIGHT, 270, BLACK);  
   Paint_SetScale(65);
-  pinMode(B1Pin, INPUT);
+  // pinMode(B1Pin, INPUT);
   home_screen();
   // graphical_data();
   // plotter();
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+
 }
+
+void get_Acceleration(){
+  float x, y, z;
+  float lowPassAcceleration = 0;
+  if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(x, y, z);
+    avgAcceleration = x+y+z;
+  }
+  Serial.println(avgAcceleration);
+}
+
 
 void home_screen(){
   Paint_DrawString_EN(20, 0, "Start", &Font16, WHITE, BLUE);
@@ -91,12 +110,13 @@ void timer(){
 void numerical_data(){
   timer();
   String velocity = String(data[count][1]);
+  String avgA = String(avgAcceleration);
   Paint_DrawString_EN(0, 0, "Avg Acc:", &Font12, BLACK, BLUE);
   Paint_DrawString_EN(0, 20, "Avg Vel:", &Font12, BLACK, BLUE);
   Paint_DrawString_EN(0, 40, "Str/Min:", &Font12, BLACK, BLUE);
   Paint_DrawString_EN(0, 60, "Dist:", &Font12, BLACK, BLUE);
   Paint_DrawString_EN(0, 80, "Time:", &Font12, BLACK, BLUE);
-  Paint_DrawString_EN(90, 0, "1", &Font12, BLACK, BLUE);
+  Paint_DrawString_EN(90, 0, avgA.c_str(), &Font12, BLACK, BLUE);
   Paint_DrawString_EN(90, 20, velocity.c_str(), &Font12, BLACK, BLUE);
   Paint_DrawString_EN(90, 40, "3", &Font12,  BLACK, BLUE);
   Paint_DrawString_EN(90, 60, "4", &Font12, BLACK, BLUE);
@@ -108,14 +128,14 @@ void graphical_data(){
   Paint_DrawLine(10, 110, 120, 110, GBLUE, DOT_PIXEL_2X2, LINE_STYLE_SOLID); // x axis
   Paint_DrawLine(10, 10, 10, 110, GBLUE, DOT_PIXEL_2X2, LINE_STYLE_SOLID); // y axis
   Paint_DrawString_EN(50, 115, "Time", &Font12, BLACK, BLUE);
-  Paint_DrawString_EN(0, 10, "V", &Font12, BLACK, BLUE);
-  Paint_DrawString_EN(0, 20, "e", &Font12, BLACK, BLUE);
-  Paint_DrawString_EN(0, 30, "l", &Font12, BLACK, BLUE);
-  Paint_DrawString_EN(0, 40, "o", &Font12, BLACK, BLUE);
+  // Paint_DrawString_EN(0, 10, "A", &Font12, BLACK, BLUE);
+  // Paint_DrawString_EN(0, 20, "c", &Font12, BLACK, BLUE);
+  Paint_DrawString_EN(0, 30, "A", &Font12, BLACK, BLUE);
+  Paint_DrawString_EN(0, 40, "c", &Font12, BLACK, BLUE);
   Paint_DrawString_EN(0, 50, "c", &Font12, BLACK, BLUE);
-  Paint_DrawString_EN(0, 60, "i", &Font12, BLACK, BLUE);
-  Paint_DrawString_EN(0, 70, "t", &Font12, BLACK, BLUE);
-  Paint_DrawString_EN(0, 80, "y", &Font12, BLACK, BLUE);
+  // Paint_DrawString_EN(0, 60, "e", &Font12, BLACK, BLUE);
+  // Paint_DrawString_EN(0, 70, "r", &Font12, BLACK, BLUE);
+  // Paint_DrawString_EN(0, 80, "y", &Font12, BLACK, BLUE);
 }
 
 void stop_screen(){
@@ -156,6 +176,13 @@ void scalePoints(float points[][2], int numPoints, float xMin, float xMax, float
     }
 }
 
+void zeroScale(float points[][2], int numPoints, float xMin, float xMax, float screenWidth){
+    for (int i = 0; i < numPoints; i++) {
+        points[i][0] = scaleValue(points[i][0], xMin, xMax, 20, screenWidth);   // Scale X
+        points[i][1] = 40; // Scale Y
+    }
+}
+
 void plotter(){
   float xMin;
   float xMax;
@@ -163,16 +190,31 @@ void plotter(){
   float yMax;
   float width =120.0;
   float height = 100.0;
-  int numPoints = sizeof(data)/sizeof(data[0]);
-  minmax(data,xMin, xMax, yMin, yMax, numPoints);
-  scalePoints(data, numPoints, xMin, xMax, yMin, yMax, width, height);
+  // int numPoints = sizeof(StrokeData)/sizeof(StrokeData[0]);
+  int numPoints = dist;
+  float plottedPoints[dist][2];
+
+  for (int i = 0; i<dist; i++){
+    for(int j = 0; j<2;j++){
+        plottedPoints[i][j] = StrokeData[i][j];
+    }
+  }
+  if (abs(plottedPoints[0][1]-plottedPoints[dist-1][1]) > 0.02){
+    minmax(plottedPoints,xMin, xMax, yMin, yMax, numPoints);
+    scalePoints(plottedPoints, numPoints, xMin, xMax, yMin, yMax, width, height);
+  }
+  else{
+    minmax(plottedPoints,xMin, xMax, yMin, yMax, numPoints);
+    zeroScale(plottedPoints, numPoints, xMin, xMax, width);
+  }
   for (int i = 0; i < numPoints; i++) {
-    Paint_DrawPoint(data[i][0], data[i][1], RED, DOT_PIXEL_2X2, DOT_FILL_AROUND);
-    Serial.println(data[i][0]);
-    Serial.println(data[i][1]);
+    Paint_DrawPoint(plottedPoints[i][0], plottedPoints[i][1], RED, DOT_PIXEL_2X2, DOT_FILL_AROUND);
+    Serial.print(plottedPoints[i][0]);
+    Serial.print(",");
+    Serial.println(plottedPoints[i][1]);
   }
   for (int i = 0; i < numPoints-1; i++) {
-    Paint_DrawLine(data[i][0], data[i][1], data[i+1][0], data[i+1][1],BLUE, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
+    Paint_DrawLine(plottedPoints[i][0], plottedPoints[i][1], plottedPoints[i+1][0], plottedPoints[i+1][1],BLUE, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
   }
 }
 
@@ -272,10 +314,13 @@ void loop() {
       stop_screen();
       break;
     case 4:
+      get_Acceleration();
       numerical_data();
       break;
     case 5:
-      graphical_data();
+      strokeLoop();
+      OLED_1in5_rgb_Clear();
+      graphical_data();  
       plotter();
       break;
   }
